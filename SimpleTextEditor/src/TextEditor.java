f472ebcdec6437a035acdf4f30384f6ff6400601
Jasmine Hurst <jasmine.ehurst@gmail.com>
2016-05-22 22:39:40 -0500

    Improvements on stack

--- SimpleTextEditor/src/TextEditor.java
+++ SimpleTextEditor/src/TextEditor.java
@@ -6,16 +6,27 @@ import java.util.regex.*;
 import java.text.StringCharacterIterator;
 
 public class TextEditor {
+    public static class basicStack{
 
+    	public static void push(String s,LinkedList list){
+    		list.add(s);
+    	}
+    	public static String pop(LinkedList list){
+    		return (String) list.getFirst();
+    	}
+    	
+    	public static void remove(LinkedList list){
+    		list.remove(0);
+    	}
+    }
     public static void main(String[] args) {
-         
+        LinkedList stack = new LinkedList();
         int numOperations = 0;
         String buffer;
         String currentOperation = "";
         Scanner scan = new Scanner(System.in);
         String textArea = "";
         String previousTextArea = "";
-
         
         StringCharacterIterator textIterator = new StringCharacterIterator(textArea, 0);
         StringCharacterIterator operationIterator = new StringCharacterIterator(currentOperation);
@@ -27,11 +38,13 @@ public class TextEditor {
             operationIterator.setText(currentOperation);
 
             if(operationIterator.first() == '1'){
+
             	previousTextArea = textArea;
             	char[] append = new char[currentOperation.length()];
             	StringBuilder myStringBuilder = new StringBuilder(currentOperation);
             	myStringBuilder.getChars(2, operationIterator.getEndIndex(), append, 0);
             	textArea = append(textArea, new String(append));
+            	basicStack.push(textArea, stack);
             	System.out.println(textArea); 
             }else if(operationIterator.first() == '2'){
             	previousTextArea = textArea;
@@ -40,16 +53,17 @@ public class TextEditor {
             	myStringBuilder.getChars(2, operationIterator.getEndIndex(), delete, 0);
             	String deletion = new String(delete);
             	textArea = delete(textArea, Integer.parseInt(deletion));
+            	basicStack.push(textArea, stack);
             	System.out.println(textArea); 
             }else if(operationIterator.first() == '3'){
-            	previousTextArea = textArea;
             	char[] print = new char[1];
             	StringBuilder myStringBuilder = new StringBuilder(currentOperation);
             	myStringBuilder.getChars(2, operationIterator.getEndIndex(), print, 0);
             	String printAt = new String(print);
             	System.out.println(textArea.charAt(Integer.parseInt(printAt)-1)); 
             }else{
-            	textArea = previousTextArea;
+            	textArea = basicStack.pop(stack);
+//            	basicStack.remove(stack);
             	System.out.println(textArea);
             }
             numOperations--;
@@ -63,9 +77,10 @@ public class TextEditor {
     	StringBuilder myStringBuilder = new StringBuilder(textArea);
     	myStringBuilder.delete(0, amount);
     	textArea = myStringBuilder.toString();
-    	return textArea;
-//    	return myStringBuilder.toString();
+    	return textArea.trim();
     }
+    
+ 
 }
 
 
